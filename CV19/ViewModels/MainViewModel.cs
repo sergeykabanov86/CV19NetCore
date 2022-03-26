60988bff807e0c7ff3352346ea0f29d679e622cc
Sergey Kabanov <sergeykabanov86@gmail.com>
2022-03-26 23:52:23 +0200

    Дизайн главного окна для студентов

--- CV19/ViewModels/MainViewModel.cs
+++ CV19/ViewModels/MainViewModel.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
+using System.Linq;
 
 using CommonLibrary.ViewModels.Base;
 using CommonLibrary.Infrastructure.Commands;
 
 using CV19.Models;
+using CV19.Models.Decanat;
 
 namespace CV19.ViewModels
 {
     internal class MainViewModel : ViewModel
     {
+
+        #region Students
+
+        public ObservableCollection<Group> Groups { get; }
+
+        #endregion
 
         #region Properties
 
@@ -57,7 +66,8 @@ namespace CV19.ViewModels
             CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
             #endregion CommandsInit
 
-            var data_points = new List<DataPoint>((int)(360/0.1));
+            #region OxyPlot
+            var data_points = new List<DataPoint>((int)(360 / 0.1));
             for (var x = 0d; x < 360; x += 0.1)
             {
                 const double to_rad = Math.PI / 180;
@@ -65,8 +75,33 @@ namespace CV19.ViewModels
 
                 data_points.Add(new DataPoint { XValue = x, YValue = y });
             }
-
             DataPoints = data_points;
+            #endregion OxyPlot
+
+            #region Students
+
+            var student_idx = 0;
+            var students = Enumerable.Range(1, 10)
+                                     .Select(i => new Student
+                                     {
+                                          Name = $"Name: {student_idx}",
+                                           SurName = $"Surname: {student_idx}",
+                                            Patronomic = $"Patronomic: {student_idx++}",
+                                             Birthday = DateTime.Now.AddDays(i*(-1) + 1),
+                                              Rating = 0
+                                     }).ToArray();
+
+            var groups = Enumerable.Range(1, 20)
+                                   .Select(i => new Group
+                                   {
+                                       Name = $"Группа {i}",
+                                       Students = students
+                                   });
+
+            Groups = new ObservableCollection<Group>(groups);
+
+
+            #endregion Students
         }
         #endregion Constructors
 
